ee99b9e8fba794747eef01809859b3660fd736ad
Group1 <hongpink473@gmail.com>
2025-03-04 13:20:02 +0700

    shipping to do

--- components/header.php
+++ components/header.php
@@ -77,7 +77,7 @@
                     </div>
 
                     <div
-                        class="icon-header-item cart-length cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart">
+                        class="icon-header-item cart-length cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="<?=count($_SESSION['cart'])?>">
                         <i class="zmdi zmdi-shopping-cart"></i>
                     </div>
 
